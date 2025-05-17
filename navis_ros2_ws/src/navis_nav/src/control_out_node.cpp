5fd4a1b98e8da0028ee336f9091e4b62071fbd5d
Michael Castiglia <michael@mcastiglia.com>
2025-05-16 20:27:18 -0400

    Added UART stuff for control node

--- navis_ros2_ws/src/navis_nav/src/control_out_node.cpp
+++ navis_ros2_ws/src/navis_nav/src/control_out_node.cpp
@@ -1,4 +1,8 @@
 #include <rclcpp/rclcpp.hpp>
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+#include <string.h>
 
 #include "navis_msgs/msg/control_out.hpp"
 
@@ -25,16 +29,52 @@ public:
 private:
 
     void open_uart() {
-        return;
+        uart_fd_ = open("/dev/ttyAMA1", O_RDWR);
+        if (uart_fd_ < 0) {
+            RCLCPP_ERROR(this->get_logger(), "Error opening UART");
+            return;
+        }
+
+        struct termios tty;
+        memset(&tty, 0, sizeof(tty));
+        if (tcgetattr(uart_fd_, &tty) != 0) {
+            RCLCPP_ERROR(this->get_logger(), "Error getting UART attributes");
+            return;
+        }
+
+        // Set UART parameters (115200 8N1)
+        cfsetospeed(&tty, B115200);
+        cfsetispeed(&tty, B115200);
+        tty.c_cflag |= (CLOCAL | CREAD);
+        tty.c_cflag &= ~CSIZE;
+        tty.c_cflag |= CS8;
+        tty.c_cflag &= ~PARENB;
+        tty.c_cflag &= ~CSTOPB;
+        tty.c_cflag &= ~CRTSCTS;
+
+        if (tcsetattr(uart_fd_, TCSANOW, &tty) != 0) {
+            RCLCPP_ERROR(this->get_logger(), "Error setting UART attributes");
+            return;
+        }
     }
 
     void close_uart() {
-        return;
+        if (uart_fd_ >= 0) {
+            close(uart_fd_);
+        }
     }
 
     // Feel free to change address, i just dont know how we're differentiating between speaker and buzzer writes
     void write_uart(int address, int val) {
-        return;
+        if (uart_fd_ < 0) return;
+
+        uint8_t packet[3] = {
+            static_cast<uint8_t>(address),
+            static_cast<uint8_t>(val),
+            static_cast<uint8_t>(address ^ val) // Simple checksum
+        };
+
+        write(uart_fd_, packet, sizeof(packet));
     }
 
     void write_callback(const navis_msgs::msg::ControlOut::SharedPtr msg) {
@@ -51,6 +91,7 @@ private:
     int buzzer_address;
     int speaker_address;
 
+    int uart_fd_ = -1;
 
 };
 
