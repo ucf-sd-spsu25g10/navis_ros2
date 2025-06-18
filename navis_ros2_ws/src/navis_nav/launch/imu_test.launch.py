48372c5a731c0783f3dde0551d51b37e5a4a6d02
Aden McKinney <aden.mckinney10@gmail.com>
2025-06-18 13:02:57 -0400

    adding ots imu driver

--- navis_ros2_ws/src/navis_nav/launch/imu_test.launch.py
+++ navis_ros2_ws/src/navis_nav/launch/imu_test.launch.py
@@ -13,9 +13,36 @@ def generate_launch_description():
 
     return LaunchDescription([
 
+        # Node(
+        #     package='navis_nav', executable='imu_publisher', output='screen',
+        #     remappings=remappings,
+        # ),
+
         Node(
-            package='navis_nav', executable='imu_publisher', output='screen',
-            remappings=remappings,
+            package="mpu9250",
+            executable="mpu9250",
+            name="mpu9250",
+            output='screen',
+            respawn=True,
+            respawn_delay=4,
+            emulate_tty=True,
+            parameters=[
+            {
+                #"print" : True,
+                "frequency" : 10,
+                "i2c_address" : 0x68,
+                "i2c_port" : 1,
+                "frame_id" : "imu_link",
+                "acceleration_scale": [1.0072387165748442, 1.0081436035838134, 0.9932769089604535],
+                "acceleration_bias": [0.17038044467587418, 0.20464685207217453, -0.12461014438322202],
+                "gyro_bias": [0.0069376404996494, -0.0619247665634732, 0.05717760948453845],
+                "magnetometer_scale": [1.0, 1.0, 1.0],
+                "magnetometer_bias": [1.335, 4.0, -2.53],
+                "magnetometer_transform": [1.0246518952703103, -0.0240401565528902, 0.0030740476998857395,
+                                            -0.024040156552890175, 0.9926708357001245, 0.002288563295390304,
+                                            0.0030740476998857356, 0.0022885632953903268, 0.9837206150979054]
+            }
+            ],
         ),
 
         Node(
