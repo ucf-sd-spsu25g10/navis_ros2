c58bd9015f98f1517a1938d6a357f83a3a708435
Aden McKinney <aden.mckinney10@gmail.com>
2025-07-11 16:56:27 -0400

    more debugging

--- navis_ros2_ws/src/navis_nav/launch/system.launch.py
+++ navis_ros2_ws/src/navis_nav/launch/system.launch.py
@@ -1,7 +1,7 @@
 import os
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
@@ -19,6 +19,11 @@ def generate_launch_description():
         package='navis_nav',
         executable='waypoint_manager',
         name='waypoint_manager'
+    )
+
+    waypoint_manager_delayed = TimerAction(
+        period=10.0,  # seconds
+        actions=[waypoint_manager]
     )
 
     # 2. Mid-Level Stereo-SLAM stack
@@ -62,14 +67,7 @@ def generate_launch_description():
         package='navis_nav',
         executable='control_action_calc'
     )
-
-    # Delay node_b until node_a exits
-    # system_delay = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=waypoint_orderer,
-    #         on_exit=[slam]
-    #     )
-    # )
+    
 
     return LaunchDescription([
         
@@ -77,11 +75,10 @@ def generate_launch_description():
         DeclareLaunchArgument('rviz',         default_value='false', description='Launch RVIZ (optional).'),
         DeclareLaunchArgument('use_raspi',    default_value='true', description='Use Raspberry Pi config if true, laptop config otherwise' ),
 
-        waypoint_manager,
+        waypoint_manager_delayed,
         slam,
         # controller_manager,
         # spawn_gpio_controller,
         # control_out_node,
         control_action_calc,
-        # system_delay
     ])
